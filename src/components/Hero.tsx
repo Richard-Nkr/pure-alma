e3613d45b4797c11b4b7f28826d6fdfa328fb3c0
Richard Nakarmi <rrichardn@yahoo.fr>
2026-07-03 18:34:09 +0200

    style: image hero en full screen (object-cover)

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -34,7 +34,7 @@ export default function Hero() {
           fill
           priority
           sizes="100vw"
-          className="object-contain"
+          className="object-cover"
         />
 
         {/* dégradé subtil pour la lisibilité de la nav */}
