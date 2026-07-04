f7f5ac058ff67528749a6879bcd2497bab294bb5
Richard Nakarmi <rrichardn@yahoo.fr>
2026-07-04 20:29:28 +0200

    Hero: mobile object-contain (dézoomé + centré), desktop object-cover

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
             fill
             priority
             sizes="100vw"
-            className="object-cover object-center"
+            className="object-contain object-center md:object-cover md:object-center"
           />
         </div>
 
