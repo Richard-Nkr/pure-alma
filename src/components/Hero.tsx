c3d47bd870dca6dbb8d8f2f103ae9d38fc4a5329
Richard Nakarmi <rrichardn@yahoo.fr>
2026-07-05 16:46:41 +0200

    style: hero full screen mobile+pc centre sur le produit

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
             fill
             priority
             sizes="100vw"
-            className="object-contain object-center md:object-cover"
+            className="object-cover object-center"
           />
         </div>
 
